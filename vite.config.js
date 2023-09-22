50622e1d2946567d254b3088ac350a655aa89632
Jeana Fernandez <jeana.luisa@gmail.com>
2023-09-22 19:35:34 +0800

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  define: {
+    'process.env': import.meta.env,
+  },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'zsbv6.js',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      assets: '/src/assets',
+      lib: '/src/lib',
+      store: '/src/store',
+      styles: '/src/styles',
+    },
+  },
+});
