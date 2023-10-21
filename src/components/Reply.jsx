11ecbde2f2e8b19887526be70605042369ade6ff
Jeana Fernandez <jeana.luisa@gmail.com>
2023-10-21 19:44:34 +0800

    created user and client message component

---
+++ src/components/Reply.jsx
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import { LikeFilled } from '@ant-design/icons';
+
+import { StyledBotReplyMessage, StyledMessage, StyledReplyFooter } from './StyledComponents';
+import { cssVariables } from 'src/styles/variables';
+import TimeStamp from './TimeStamp';
+
+const Reply = ({ message }) => {
+  const { timeReply, reply } = message;
+  console.log('timeReply', timeReply);
+  return (
+    <StyledMessage>
+      <StyledBotReplyMessage>
+        <span>{reply}</span>
+        <StyledReplyFooter>
+          <em>
+            <TimeStamp time={timeReply} />
+          </em>
+          <LikeFilled style={{ color: cssVariables.blueLike }} />
+        </StyledReplyFooter>
+      </StyledBotReplyMessage>
+    </StyledMessage>
+  );
+}
+
+Reply.propTypes = {
+  message: PropTypes.shape({
+    reply: PropTypes.string,
+    timeReply: PropTypes.any,
+  })
+};
+
+export default Reply
---
+++ src/components/UserMessage.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+import { StyledClientMessage, StyledMessage } from './StyledComponents';
+import TimeStamp from './TimeStamp';
+
+const UserMessage = ({ message }) => {
+  const { timeMessageSent, text } = message;
+  
+  return (
+    <StyledMessage>
+      <StyledClientMessage>
+        <span>{text}</span>
+        <em><TimeStamp time={timeMessageSent} /></em>
+      </StyledClientMessage>
+    </StyledMessage>
+  );
+}
+
+UserMessage.propTypes = {
+  message: PropTypes.shape({
+    text: PropTypes.string,
+    timeMessageSent: PropTypes.any,
+  }),
+};
+
+export default UserMessage
