c26e2462d81f55eff290959ff2e35232cd8a14a7
Jeana Fernandez <jeana.luisa@gmail.com>
2023-10-23 11:45:15 +0800

    added typing message component

---
+++ src/components/Typing.jsx
@@ -0,0 +1,15 @@
+import { StyledBotReplyMessage, StyledMessage } from './StyledComponents';
+
+const Typing = () => {
+  return (
+    <StyledMessage>
+      <StyledBotReplyMessage className="typing">
+        <span></span>
+        <span></span>
+        <span></span>
+      </StyledBotReplyMessage>
+    </StyledMessage>
+  );
+}
+
+export default Typing
