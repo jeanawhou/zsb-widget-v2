1fe7e222042da3b4f06837a1feb16e3be9303fa4
Jeana Fernandez <jeana.luisa@gmail.com>
2023-11-23 20:31:10 +0800

    fix timestamp rerendering

--- src/components/TimeStamp.jsx
+++ src/components/TimeStamp.jsx
@@ -4,6 +4,7 @@ import { getTimeDifference } from 'src/utils/datetime';
 
 const TimeStamp = props => {
   const oneMinute = 1000 * 60;
+  const oneHour = oneMinute * 60;
   const { time } = props
   const [timeAgo, setTimeAgo] = useState(getTimeDifference(time));
   const [counter, setCounter] = useState(0);
@@ -15,14 +16,19 @@ const TimeStamp = props => {
   }, [counter, time]);
 
   useEffect(() => {
+    const minuteAgo = timeAgo && (timeAgo.endsWith('seconds ago') || timeAgo.endsWith('m ago'))
     const intervalId = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, oneMinute);
+      if (minuteAgo) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else if (timeAgo && timeAgo.endsWith('h ago')) {
+        setCounter((prevCounter) => prevCounter + 1);
+      }
+    }, minuteAgo ? oneMinute : oneHour);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, [time]);
 
   return <>{timeAgo}</>;
 }
