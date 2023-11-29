c39bdcf9af7342f1923f60a769ce665e7ff0e1a1
Jeana Fernandez <jeana.luisa@gmail.com>
2023-11-29 08:35:44 +0800

    added prettier husky and lint-stage

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  proseWrap: 'always',
+  arrowParens: 'always',
+  trailingComma: 'es5',
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  semiColon: 'always'
+};
