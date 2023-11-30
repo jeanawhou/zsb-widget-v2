e4a6713dff6b371888e1d86d5d68da28c9b1ab27
Jeana Fernandez <jeana.luisa@gmail.com>
2023-11-30 08:34:24 +0800

    add precommit scripts and ignore files

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  proseWrap: 'always',
-  arrowParens: 'always',
-  trailingComma: 'es5',
-  arrowParens: 'always',
-  endOfLine: 'auto',
-  semiColon: 'always'
-};
