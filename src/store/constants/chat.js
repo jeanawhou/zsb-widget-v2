951fe081c5391ebd9093d92af98c5d60b09c0ef2
Jeana Fernandez <jeana.luisa@gmail.com>
2023-12-11 10:41:51 +0800

    add bot and intergration selectors

---
+++ src/store/constants/chat.js
@@ -0,0 +1,12 @@
+export const ZSB_CHAT_BREAKER_ENCONDING = '&lt;zsb-chat-breaker /&gt;';
+
+export const REACTIONS = [
+  {
+    display: 'like',
+    reply: 1,
+  },
+  {
+    display: 'dislike',
+    reply: 0,
+  },
+];
---
+++ src/store/selectors/integration.js
@@ -0,0 +1,1 @@
+export const integrationSelector = (state) => state.integration;
