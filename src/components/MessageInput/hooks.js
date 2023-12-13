313cf392f7e9063f6a5051e28b885b862c7d3128
Jeana Fernandez <jeana.luisa@gmail.com>
2023-12-13 11:16:45 +0800

    remove hard coded reply

--- src/components/MessageInput/hooks.js
+++ src/components/MessageInput/hooks.js
@@ -1,38 +1,39 @@
 import { useContext, useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 
 import { Context } from 'store/store';
 import { MINIMIZE_WIDGET } from 'store/action';
-import { ADD_REPLY, SEND_NEW_MESSAGE } from '../../store/action';
+import { ADD_ERROR_REPLY, ADD_REPLY, SEND_NEW_MESSAGE } from '../../store/action';
 import useSelector from 'store/useSelector';
 import { isWidgetExpandedSelector } from 'store/selectors/ui.js';
 import { apiService } from 'src/services/api.service';
 import { publicKeysSelector } from 'src/store/selectors';
 import { userSelector } from 'src/store/selectors/user';
-import { botIdentifierSelector } from 'src/store/selectors/bot';
+import { integrationSelector } from 'src/store/selectors/integration';
+import { generateUUID } from 'src/store/utils';
 
 export const useMessageInput = () => {
-  const [state, dispatch] = useContext(Context);
+  const [, dispatch] = useContext(Context);
   const [newMessage, setNewMessage] = useState('');
   const isExpanded = useSelector(isWidgetExpandedSelector);
   const publicKeys = useSelector(publicKeysSelector);
   const user = useSelector(userSelector);
-  const botIdentifier = useSelector(botIdentifierSelector);
+  const integration = useSelector(integrationSelector);
 
   const handleSendMessage = async (e) => {
     e.preventDefault();
     if (newMessage.trim() === '') return;
-    const interactionId = uuidv4();
-    dispatch({ type: SEND_NEW_MESSAGE, payload: { newMessage } });
+    const interactionId = generateUUID();
+    dispatch({ type: SEND_NEW_MESSAGE, payload: { newMessage, interactionId } });
     setNewMessage('');
-    const res = await apiService.askQuestion(newMessage, publicKeys, user, interactionId, botIdentifier);
-    if (res.data.success && res.data.report[0]) {
-      const reply = res.data.report[0];
-      console.log('reply', reply);
+    try {
+      const res = await apiService.askQuestion(newMessage, publicKeys, user, interactionId, integration);
+      if (res.data.success && res.data.report[0]) {
+        const reply = res.data.report[0];
+        addResponse(reply);
+      }
+    } catch (error) {
+      dispatch({ type: ADD_ERROR_REPLY });
     }
-    addResponse(
-      `Hi there! Ask me a question and I'll find the answer for you. Hi there! Ask me a question and I'll find the answer for you.Hi there! Ask me a question and I'll find the answer for you.Hi there! Ask me a question and I'll find the answer for you.`,
-    );
   };
 
   const addResponse = (reply) => {
