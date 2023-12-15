1ee83e53a83aa7820a5423d99a0ee3d6f52fc2bb
Jeana Fernandez <jeana.luisa@gmail.com>
2023-12-15 16:55:48 +0800

    fix maxdislikes reached selector

--- src/store/selectors/index.js
+++ src/store/selectors/index.js
@@ -10,7 +10,9 @@ export const metadataSelector = (state) => state.metadata;
 
 export const dislikesSelector = createSelector(metadataSelector, (metadata) => metadata.dislikes);
 
-export const isMaxDislikesReachedSelector = createSelector(
-  metadataSelector,
-  (metadata) => metadata.dislikes >= metadata.maxDislikes,
-);
+export const isMaxDislikesReachedSelector = createSelector(metadataSelector, (metadata) => {
+  if (metadata.maxDislikes && metadata.dislikes) {
+    return metadata.dislikes >= metadata.maxDislikes;
+  }
+  return false;
+});
