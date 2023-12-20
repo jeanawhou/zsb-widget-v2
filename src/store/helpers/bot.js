69dc7eb5db6bf888a5c532307ace6892ae5286d9
Jeana Fernandez <jeana.luisa@gmail.com>
2023-12-20 12:47:54 +0800

    prettier

--- src/store/helpers/bot.js
+++ src/store/helpers/bot.js
@@ -1,4 +1,4 @@
-import { camelCaseKeys } from "src/utils";
+import { camelCaseKeys } from 'src/utils';
 
 export const extractPublicKeys = (pub) => {
   return {
@@ -7,13 +7,14 @@ export const extractPublicKeys = (pub) => {
     wlk: pub.wlk,
     key: pub.key,
   };
-}
+};
 
 export const extractWidgetUI = (config = {}, props = {}) => {
   const configProps = camelCaseKeys(config);
   const widgetProps = camelCaseKeys(props);
   return {
+    // overwrite config props with component props
     ...configProps,
-    ...widgetProps
+    ...widgetProps,
   };
 };
