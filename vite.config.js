c1cc9acde0f237ce11c60c4f7c8f05f6e281e382
Jeana Fernandez <jeana.luisa@gmail.com>
2024-02-12 15:02:42 +0800

    remove un supported plugin

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv, splitVendorChunkPlugin } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [react(), splitVendorChunkPlugin()],
+    plugins: [react()],
     define: defineEnv,
     build: {
       outDir: 'dist',
