9323eafeb19ef7972bc17699c5f7a52f61b3f981
Jeana Fernandez <jeana.luisa@gmail.com>
2024-02-20 12:50:10 +0800

    fix small letters component props

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,18 @@
 export const ALPHANUMBERIC_REGEX = /[^a-zA-Z0-9]/g;
 
 export const MOBILE_USER_AGENT_REGEX = /Mobi|Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+
+export const COMPONENT_PROPS = [
+  'bot',
+  'color',
+  'textColor',
+  'height',
+  'showCloseButton',
+  'openWidget',
+  'headerResponseImgUrl',
+  'widgetIconUrl',
+  'fontSize',
+  'fontStyle',
+  'bubbleColor',
+  'bubbleGradient',
+];
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,20 @@
 import { mapValues, isPlainObject, camelCase, mapKeys } from 'lodash';
 import { ZSB_CHAT_BREAKER_ENCONDING } from 'src/constants/chat';
+import { COMPONENT_PROPS } from 'src/constants/index';
+
+const _camelCaseKeys = (key) => {
+  const componentPropIndex = COMPONENT_PROPS.findIndex((prop) => prop.toLowerCase() === key.toLowerCase());
+  if (componentPropIndex >= 0) {
+    return COMPONENT_PROPS[componentPropIndex];
+  } else {
+    return camelCase(key);
+  }
+};
 
 export const camelCaseKeys = (obj) => {
   if (isPlainObject(obj)) {
     return mapValues(
-      mapKeys(obj, (_, key) => camelCase(key)),
+      mapKeys(obj, (_, key) => _camelCaseKeys(key)),
       (value) => {
         if (isPlainObject(value)) {
           return camelCaseKeys(value);
