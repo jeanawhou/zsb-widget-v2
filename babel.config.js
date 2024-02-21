bf9349502dd9d96100e3ff6fcde52159ac2cb045
Jeana Fernandez <jeana.luisa@gmail.com>
2024-02-21 12:18:54 +0800

    Merge branch version/node-16;

---
+++ babel.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+module.exports = {
+  presets: ['@babel/preset-env', '@babel/preset-react'],
+  plugins: [
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-transform-nullish-coalescing-operator"
+  ]
+};
