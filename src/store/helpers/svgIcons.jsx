41e2a3c8bf8072bd3f52d5a7b9328b41b1cb0c61
Jeana Fernandez <jeana.luisa@gmail.com>
2024-02-27 13:28:19 +0800

    consolidate icons with old version icon names

--- src/store/helpers/svgIcons.jsx
+++ src/store/helpers/svgIcons.jsx
@@ -1,10 +1,10 @@
-import { icon1, icon2, icon3, icon4, DefaultIcon } from 'src/svg/Icons';
+import { icon1, icon3, icon2, icon4, defaultIcon } from 'src/svg/Icons';
 
 export const extractUserIcon = (icon, color) => {
   switch (icon) {
     case 'icon1': {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 31" width={40} height={40}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -2.9 24 30" width={45} height={48}>
           {icon1(color)}
         </svg>
       );
@@ -12,7 +12,7 @@ export const extractUserIcon = (icon, color) => {
 
     case 'icon2': {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -2.9 24 30" width={45} height={48}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2.2 -1.8 28 25" width={45} height={48}>
           {icon2(color)}
         </svg>
       );
@@ -20,7 +20,7 @@ export const extractUserIcon = (icon, color) => {
 
     case 'icon3': {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4.5 -.5 33 30" width={45} height={48}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-5 1 35 24" width={45} height={48}>
           {icon3(color)}
         </svg>
       );
@@ -28,13 +28,19 @@ export const extractUserIcon = (icon, color) => {
 
     case 'icon4': {
       return (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-2.9 -1 30 30'" width={45} height={48}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-4.5 -.5 33 30" width={45} height={48}>
           {icon4(color)}
         </svg>
       );
     }
-
+    case 'default': {
+      return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-5 6 45 20" width={40} height={40}>
+          {defaultIcon(color)}
+        </svg>
+      );
+    }
     default:
-      return DefaultIcon(color);
+      return icon;
   }
 };
