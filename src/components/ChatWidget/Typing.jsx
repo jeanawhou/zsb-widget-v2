3b627f9017324b762528e588d747c21d551dcfce
Jeana Fernandez <jeana.luisa@gmail.com>
2024-02-29 10:30:35 +0800

    only show typing if no bot answer yet or typing experience is enabled

--- src/components/ChatWidget/Typing.jsx
+++ src/components/ChatWidget/Typing.jsx
@@ -1,20 +1,26 @@
 import useSelector from 'src/store/useSelector';
 import { StyledBotReply, StyledMessage } from './StyledComponents';
-import { clientBubbleColorSelector, fontSizeSelector, replyBubbleColorSelector } from 'src/store/selectors/ui';
+import {
+  clientBubbleColorSelector,
+  fontSizeSelector,
+  isTypingSelector,
+  replyBubbleColorSelector,
+} from 'src/store/selectors/ui';
 
 const Typing = () => {
   const replyBubbleGradient = useSelector(replyBubbleColorSelector);
   const clientBubbleColor = useSelector(clientBubbleColorSelector);
   const fontSize = useSelector(fontSizeSelector);
-  return (
-    <StyledMessage fontsize={fontSize}>
+  const isTyping = useSelector(isTypingSelector);
+  return isTyping ? (
+    <StyledMessage $fontsize={fontSize}>
       <StyledBotReply color={replyBubbleGradient} dotcolor={clientBubbleColor} className="typing">
         <span></span>
         <span></span>
         <span></span>
       </StyledBotReply>
     </StyledMessage>
-  );
+  ) : null;
 };
 
 export default Typing;
