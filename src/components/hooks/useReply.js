fe122acb75d1cc35c73bf60aaf4d9d90d84028a7
Jeana Fernandez <jeana.luisa@gmail.com>
2024-03-06 11:15:26 +0800

    remove delay for multiple chat bubble if typingExp not enabled

--- src/components/hooks/useReply.js
+++ src/components/hooks/useReply.js
@@ -2,6 +2,7 @@ import { isPlainObject } from 'lodash';
 import { useContext } from 'react';
 import { ADD_ERROR_REPLY, ADD_REPLY } from 'src/store/action';
 import { messagesSelector } from 'src/store/selectors/messages';
+import { typingExperienceEnabledSelector } from 'src/store/selectors/ui';
 import { Context } from 'src/store/store';
 import useSelector from 'src/store/useSelector';
 import { getMessageReplies } from 'src/utils/messages';
@@ -9,30 +10,47 @@ import { getMessageReplies } from 'src/utils/messages';
 const useReply = () => {
   const [, dispatch] = useContext(Context);
   const allMessages = useSelector(messagesSelector);
+  const typingExperienceEnabled = useSelector(typingExperienceEnabledSelector);
+
+  const addReply = (messageReplies, msg, index, answerObj) => {
+    const { context, ...restOfResponse } = answerObj;
+    if (typingExperienceEnabled) {
+      setTimeout(
+        () => {
+          dispatch({
+            type: ADD_REPLY,
+            payload: {
+              ...restOfResponse,
+              context: context || {},
+              // text prop will be the bot's answer
+              text: msg,
+              newMessageCount: index + 1,
+              isLastReplyItem: index === messageReplies.length - 1,
+            },
+          });
+        },
+        (index + 1) * 1200,
+      ); // Slightly more than 2s to account for the typing effect
+    } else {
+      dispatch({
+        type: ADD_REPLY,
+        payload: {
+          context: {},
+          // text prop will be the bot's answer
+          text: msg,
+          newMessageCount: index + 1,
+          isLastReplyItem: index === messageReplies.length - 1,
+        },
+      });
+    }
+  };
 
   const addResponse = (answer) => {
     if (isPlainObject(answer)) {
-      const { context, ...restOfResponse } = answer;
+      const { context } = answer;
       const messageReplies = getMessageReplies(context);
       messageReplies.forEach((msg, index) => {
-        setTimeout(
-          () => {
-            dispatch({
-              type: ADD_REPLY,
-              payload: {
-                ...restOfResponse,
-                context: {
-                  ...context,
-                },
-                // text prop will be the bot's answer
-                text: msg,
-                newMessageCount: index + 1,
-                isLastReplyItem: index === messageReplies.length - 1,
-              },
-            });
-          },
-          (index + 1) * 1200,
-        ); // Slightly more than 2s to account for the typing effect
+        addReply(messageReplies, msg, index, answer);
       });
     } else if (typeof answer === 'string') {
       // find the message with same answer
@@ -40,21 +58,7 @@ const useReply = () => {
       // get the reply prop
       const messageReplies = messageWithMatchedAnswer?.reply?.text;
       messageReplies.forEach((msg, index) => {
-        setTimeout(
-          () => {
-            dispatch({
-              type: ADD_REPLY,
-              payload: {
-                context: {},
-                // text prop will be the bot's answer
-                text: msg,
-                newMessageCount: index + 1,
-                isLastReplyItem: index === messageReplies.length - 1,
-              },
-            });
-          },
-          (index + 1) * 1200,
-        ); // Slightly more than 2s to account for the typing effect
+        addReply(messageReplies, msg, index, answer);
       });
     } else {
       dispatch({
